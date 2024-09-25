844bb3b1b11a5e3cbccff68bcabc05eff0e8024b
SahilMohite <109684495+SahilMohite@users.noreply.github.com>
2024-09-25 11:00:38 +0530

    routes added

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
-
-// Import your local image
-import image1 from '../assets/images/my photo.jpg'; // Use the correct path to your image
+import { useNavigate } from "react-router-dom";
+import image1 from '../assets/images/my photo.jpg'; 
 
 const BackgroundSection: React.FC = () => {
+
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate("/contact"); 
+  };
+
   return (
     <div
       className="h-screen bg-fixed bg-cover"
@@ -15,9 +21,12 @@ const BackgroundSection: React.FC = () => {
       <div className="flex flex-col items-center justify-center h-full bg-black bg-opacity-50">
         <h2 className="text-white text-5xl sm:text-6xl font-serif font-bold tracking-wide text-center">Sahil Mohite</h2>
         <h1 className="text-white text-lg sm:text-xl font-serif text-center">React JS | Front-End | UI/UX</h1>
-        <button className="mt-4 px-6 py-2 bg-transparent border-2 border-white text-white font-semibold rounded-lg hover:bg-white hover:text-black transition duration-200">
-          Contact Me
-        </button>
+        <button
+      className="mt-4 px-6 py-2 bg-transparent border-2 border-white text-white font-semibold rounded-lg hover:bg-white hover:text-black transition duration-200"
+      onClick={handleButtonClick}
+    >
+      Contact Me
+    </button>
       </div>
     </div>
   );

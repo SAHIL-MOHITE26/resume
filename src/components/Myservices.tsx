77950720a14a3f126fad07a6c5e499aff8893209
SAHIL-MOHITE26 <sahil@kshantechsoft.com>
2024-10-17 15:18:49 +0530

    marginal changes

--- src/components/Myservices.tsx
+++ src/components/Myservices.tsx
@@ -25,7 +25,7 @@ const services = [
 const Myservices: React.FC = () => {
   return (
     <div className="container mx-auto p-8 bg-gradient-to-b from-white to-gray-400">
-      <h1 className="text-5xl mb-6 text-black text-center mt-[-100px]">Services</h1>
+      <h1 className="text-5xl mb-6 text-black text-center mt-[-120px]">Services</h1>
       <div className="ml-20 mt-10 mb-40 grid grid-cols-1 md:grid-cols-3 gap-8 w-[1300px] ">
         {services.map(service => (
           <div key={service.id} className="border rounded p-[60px] text-center transition-transform duration-300 hover:shadow-lg">

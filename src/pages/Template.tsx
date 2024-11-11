07dd2186ae7d0fc4778dc7e71371806fcbe5a1f1
SAHIL-MOHITE26 <sahil@kshantechsoft.com>
2024-11-11 14:40:35 +0530

    linking

--- src/pages/Template.tsx
+++ src/pages/Template.tsx
@@ -1,13 +1,13 @@
 const cardData = [
-    { id: 1, imageUrl: 'https://colorlib.com/wp/wp-content/uploads/sites/2/glint-free-fullscreen-small-business-template.jpg.webp', reviewLink: 'https://colorlib.com/wp/template/glint/' },
-    { id: 2, imageUrl: 'https://colorlib.com/wp/wp-content/uploads/sites/2/nitro-free-template-353x278.jpg.avif', reviewLink: 'https://colorlib.com/wp/template/nitro/' },
-    { id: 3, imageUrl: 'https://colorlib.com/wp/wp-content/uploads/sites/2/rezume-free-template.jpg.avif', reviewLink: 'https://colorlib.com/wp/template/rezume/' },
-    { id: 4, imageUrl: 'https://colorlib.com/wp/wp-content/uploads/sites/2/rezume-free-template.jpg.avif', reviewLink: 'https://colorlib.com/wp/template/rezume/' },
-    { id: 5, imageUrl: 'https://colorlib.com/wp/wp-content/uploads/sites/2/orbit-free-template.jpg.avif', reviewLink: 'https://colorlib.com/wp/template/orbit/' },
-    { id: 6, imageUrl: 'https://colorlib.com/wp/wp-content/uploads/sites/2/busicol-free-template.jpg.avif', reviewLink: 'https://colorlib.com/wp/template/busicol/' },
-    { id: 7, imageUrl: 'https://colorlib.com/wp/wp-content/uploads/sites/2/skater-free-template.jpg.avif', reviewLink: 'https://colorlib.com/wp/template/skater/' },
-    { id: 8, imageUrl: 'https://colorlib.com/wp/wp-content/uploads/sites/2/myphotography-free-template.jpg.avif', reviewLink: 'https://colorlib.com/wp/template/myphotography/' },
-    { id: 9, imageUrl: 'https://colorlib.com/wp/wp-content/uploads/sites/2/eroo-colorlib-template.jpg.avif', reviewLink: 'https://colorlib.com/wp/template/myphotography/' },
+    { id: 1, imageUrl: 'https://colorlib.com/wp/wp-content/uploads/sites/2/glint-free-fullscreen-small-business-template.jpg.webp', reviewLink: 'https://colorlib.com/wp/template/glint/',chooseLink:'/contact' },
+    { id: 2, imageUrl: 'https://colorlib.com/wp/wp-content/uploads/sites/2/nitro-free-template-353x278.jpg.avif', reviewLink: 'https://colorlib.com/wp/template/nitro/' ,chooseLink:'/contact'},
+    { id: 3, imageUrl: 'https://colorlib.com/wp/wp-content/uploads/sites/2/rezume-free-template.jpg.avif', reviewLink: 'https://colorlib.com/wp/template/rezume/' ,chooseLink:'/contact'},
+    { id: 4, imageUrl: 'https://colorlib.com/wp/wp-content/uploads/sites/2/rezume-free-template.jpg.avif', reviewLink: 'https://colorlib.com/wp/template/rezume/',chooseLink:'/contact' },
+    { id: 5, imageUrl: 'https://colorlib.com/wp/wp-content/uploads/sites/2/orbit-free-template.jpg.avif', reviewLink: 'https://colorlib.com/wp/template/orbit/',chooseLink:'/contact' },
+    { id: 6, imageUrl: 'https://colorlib.com/wp/wp-content/uploads/sites/2/busicol-free-template.jpg.avif', reviewLink: 'https://colorlib.com/wp/template/busicol/',chooseLink:'/contact' },
+    { id: 7, imageUrl: 'https://colorlib.com/wp/wp-content/uploads/sites/2/skater-free-template.jpg.avif', reviewLink: 'https://colorlib.com/wp/template/skater/',chooseLink:'/contact' },
+    { id: 8, imageUrl: 'https://colorlib.com/wp/wp-content/uploads/sites/2/myphotography-free-template.jpg.avif', reviewLink: 'https://colorlib.com/wp/template/myphotography/' ,chooseLink:'/contact'},
+    { id: 9, imageUrl: 'https://colorlib.com/wp/wp-content/uploads/sites/2/eroo-colorlib-template.jpg.avif', reviewLink: 'https://colorlib.com/wp/template/myphotography/',chooseLink:'/contact' },
   ];
   
   const Templates = () => {
@@ -33,9 +33,9 @@ const cardData = [
       
               {/* Buttons section */}
               <div className="flex justify-between">
-                <button className="px-11 py-2 bg-blue-500 text-white hover:bg-blue-600 transition-all duration-200 ml-2">
+                <a href={card.chooseLink} target="_blank" rel="noopener noreferrer" className="px-11 py-2 bg-blue-500 text-white hover:bg-blue-600 transition-all duration-200 ml-2">
                   Choose
-                </button>
+                </a>
                 <a href={card.reviewLink} target="_blank" rel="noopener noreferrer" className="px-12 py-2 bg-gray-500 text-white hover:bg-gray-600 transition-all duration-200 mr-2">
                   Review
                 </a>

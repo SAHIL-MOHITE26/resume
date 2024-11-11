c6f4d2a9f9128aa23b56bd76d9828fe85d02a7db
SAHIL-MOHITE26 <sahil@kshantechsoft.com>
2024-11-11 14:53:15 +0530

    edit ui

--- src/pages/Template.tsx
+++ src/pages/Template.tsx
@@ -12,22 +12,22 @@ const cardData = [
   
   const Templates = () => {
     return (
-      <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100">
+      <div className="flex flex-col justify-center items-center min-h-screen bg-gradient-to-r from-gray-800 via-gray-700 to-gray-600">
         
         {/* Heading Section */}
-        <h1 className="text-4xl font-bold mt-10">Choose Your Templates</h1> {/* Centered Heading */}
-        
+        <h1 className="text-4xl text-white font-bold mt-10">Choose Your Template</h1> {/* Centered Heading */}
+        <hr className="px-20 mt-3 mb-[-24px]"></hr>
         {/* Cards Section */}
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 p-8 w-[1200px]">
           {cardData.map((card) => (
             <div 
               key={card.id} 
-              className="bg-white shadow-lg rounded-lg p-6 transform transition-all duration-300 hover:shadow-xl hover:scale-105 overflow-hidden" // Card hover effects
+              className="p-6 transform transition-all duration-300  hover:scale-105 overflow-hidden" // Card hover effects
             >
               <div className="overflow-hidden rounded-md mb-4"> {/* Image container */}
                 <img 
                   src={card.imageUrl} 
-                  className="w-full h-56 object-cover transition-transform duration-300 hover:scale-110" // Image hover zoom effect
+                  className="w-full h-56 object-fill transition-transform duration-300 hover:scale-110" // Image hover zoom effect
                 />
               </div>
       
@@ -36,7 +36,7 @@ const cardData = [
                 <a href={card.chooseLink} target="_blank" rel="noopener noreferrer" className="px-11 py-2 bg-blue-500 text-white hover:bg-blue-600 transition-all duration-200 ml-2">
                   Choose
                 </a>
-                <a href={card.reviewLink} target="_blank" rel="noopener noreferrer" className="px-12 py-2 bg-gray-500 text-white hover:bg-gray-600 transition-all duration-200 mr-2">
+                <a href={card.reviewLink} target="_blank" rel="noopener noreferrer" className=" px-12 py-2 bg-gray-500 text-white hover:bg-gray-600 transition-all duration-200 mr-2">
                   Review
                 </a>
               </div>
